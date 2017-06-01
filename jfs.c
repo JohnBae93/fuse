8754234c1ff98243a6d2843a0b8d05eb3eecf428
JohnBae93 <JohnBae93@naver.com>
2017-06-01 00:14:25 -0700

    make function

--- jfs.c
+++ jfs.c
@@ -13,8 +13,52 @@ typedef struct jnode {
     struct jnode *child;
 }JNODE;
 
+static int jfs_getattr(){
+}
+static int jfs_readdir(){
+}
+static int jfs_mkdir(){
+}
+static int jfs_chmod(){
+}
+static int jfs_open(){
+}
+static int jfs_release(){
+}
+static int jfs_rmdir(){
+}
+static int jfs_rename(){
+}
+static int jfs_read(){
+}
+static int jfs_create(){
+}
+static int jfs_ultimens(){
+}
+static int jfs_unlink(){
+}
+static int jfs_write(){
+}
+static int truncate(){
+}
+
+static struct fuse_operation jfs_oper = {
+    .getattr = ;
+    .readdir = ;
+    .mkdir = ;
+    .chmod = ;
+    .open = ;
+    .release = ;
+    .rmdir = ;
+    .rename = ;
+    .read = ;
+    .create = ;
+    .utimens = ;
+    .unlink = ;
+    .write = ;
+    .truncate = ;
+};
 int main(){
-    JNODE jnode;
-    jnode.st.st_ino = 6;
-    printf("inode number : %d", jnode.st.st_ino);   
+
+            
 }
