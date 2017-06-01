be39053ca237114456ae3c6fd146e92bf9863b21
John <johnbae57@naver.com>
2017-06-01 22:44:29 +0900

    "6/10 23pm"

--- jfs.c
+++ jfs.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
-//check
+
 typedef struct jnode {
     struct stat st;
     char *fname;
@@ -17,21 +17,22 @@ typedef struct jnode {
 char *get_leaf_name(char *path) {
     int len = strlen(path);
     char *name = path[len - 1];
-    
-    while(*name != '/') {
+
+    while (*name != '/') {
         name--;
     }
     name++;
 
     return name;
 }
+
 void insert_node(JNODE *parent_node, JNODE *new_node) {
     ////// check ENOTDIR
-    
+
     JNODE *tmp;
-    if(parent_node->child == NULL || *(parent_node->child) == NULL)
+    if (parent_node->child == NULL || *(parent_node->child) == NULL)
         parent_node->child = new_node;
-    else { 
+    else {
         tmp = parent_node->child;
         parent_node->child = new_node;
         new_node->next = tmp;
@@ -39,18 +40,18 @@ void insert_node(JNODE *parent_node, JNODE *new_node) {
 
 }
 
-static int jfs_getattr(const char *path, struct stat *stbuf){
+static int jfs_getattr(const char *path, struct stat *stbuf) {
     int ret = 0;
-    
+
     memset(stbuf, 0, sizeof(struct stat));
 
-    JNODE *node = search_node_path /////
+    JNODE *node = search_node(path);
     ////////   
-    if(node) {
+    if (node) {
         stbuf->st_ino = node->st.st_ino;
         stbuf->st_mode = node->st.st_mode;
         stbuf->st_nlink = node->st.nlink;
-        stbuf->st_uid = node->st.st_uid;     
+        stbuf->st_uid = node->st.st_uid;
         stbuf->st_gid = node->st.st_gid;
         stbuf->st_rdev = node->st.st_rdev;
         stbuf->st_size = node->st.st_size;
@@ -58,15 +59,17 @@ static int jfs_getattr(const char *path, struct stat *stbuf){
         stbuf->st_mtime = node->st.st_mtime;
         stbuf->st_xtime = node->st.st_xtime;
     } else {
-        return -ENOENT
+        return -ENOENT;
     }
 
 }
-static int jfs_readdir(){
+
+static int jfs_readdir() {
 }
-static int jfs_mkdir(const char *path, mode_t mode){
+
+static int jfs_mkdir(const char *path, mode_t mode) {
     char *fname = get_leaf_name(path);
-    
+
     // EEXIST 
     // ENOENT 
     char *parent_path = get_parent_path(path);
@@ -74,50 +77,62 @@ static int jfs_mkdir(const char *path, mode_t mode){
     JNODE *new_node = make_jnode();/////
     JNODE *parent_node = search_node(parent_path);
     insert_node(parent_node, new_node);//////
-    
+
     return 0;
 }
-static int jfs_chmod(){
+
+static int jfs_chmod() {
 }
-static int jfs_open(){
+
+static int jfs_open() {
 }
-static int jfs_release(){
+
+static int jfs_release() {
 }
-static int jfs_rmdir(){
+
+static int jfs_rmdir() {
 }
-static int jfs_rename(){
+
+static int jfs_rename() {
 }
-static int jfs_read(){
+
+static int jfs_read() {
 }
-static int jfs_create(){
+
+static int jfs_create() {
 }
-static int jfs_ultimens(){
+
+static int jfs_ultimens() {
 }
-static int jfs_unlink(){
+
+static int jfs_unlink() {
 }
-static int jfs_write(){
+
+static int jfs_write() {
 }
-static int jfs_truncate(){
+
+static int jfs_truncate() {
 }
 
 static struct fuse_operation jfs_oper = { // flush?
-    .getattr = jfs_getattr,
-    .readdir = jfs_readdir,
-    .mkdir = jfs_mkdir,
-    .chmod = jfs_chmod,
-    .open = jfs_open,
-    .release = jfs_release,
-    .rmdir = jfs_rmdir,
-    .rename = jfs_rename,
-    .read = jfs_read,
-    .create = jfs_create,
-    .utimens = jfs_utimens,
-    .unlink = jfs_unlink,
-    .write = jfs_write,
-    .truncate = jfs_truncate,
+        .getattr = jfs_getattr,
+        .readdir = jfs_readdir,
+        .mkdir = jfs_mkdir,
+        .chmod = jfs_chmod,
+        .open = jfs_open,
+        .release = jfs_release,
+        .rmdir = jfs_rmdir,
+        .rename = jfs_rename,
+        .read = jfs_read,
+        .create = jfs_create,
+        .utimens = jfs_utimens,
+        .unlink = jfs_unlink,
+        .write = jfs_write,
+        .truncate = jfs_truncate,
 };
-int main(int argc, char*argv[]){
 
-   return fuse_main(argc, argv, &jfs_oper, NULL);
-            
+int main(int argc, char *argv[]) {
+
+    return fuse_main(argc, argv, &jfs_oper, NULL);
+
 }
