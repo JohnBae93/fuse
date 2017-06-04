4969a57a2452d1f6f7762fed04faf47487887b28
aih8345 <aih9599@gmail.com>
2017-06-04 23:15:50 +0900

    6/4 data part tested

--- jfs.c
+++ jfs.c
@@ -234,74 +234,87 @@ int delete_jnode(JNODE *node) {
  * data function
  */
 void insert_data(DATA *node) {
-    if (dptr.head == NULL) {
-        dptr.head = node;
-        dptr.tail = node;
-    } else { //always insert at the end of list
-        dptr.tail->next = node;
-        node->pre = dptr.tail;
-        dptr.tail = node;
-    }
+	if (dptr.head == NULL) {
+		dptr.head = node;
+		dptr.tail = node;
+	}
+	else { //always insert at the end of list
+		dptr.tail->next = node;
+		node->pre = dptr.tail;
+		dptr.tail = node;
+	}
 }
 
 void del_data(DATA *dnode) {
 
-    if (dptr.head == dnode) { //dnode is first node
-        dptr.head = dptr.head->next;
-        if (dptr.head != NULL) { //dnode has next node
-            dptr.head->pre = NULL;
-            dnode->pre = NULL;
-            dnode->next = NULL;
-        }
-    } else {
-        DATA *temp = dnode;
-        if (dnode->pre != NULL) {
-            dnode->pre->next = temp->next;
-        }
-        if (dnode->next != NULL) {
-            dnode->next->pre = temp->pre;
-        }
-        dnode->pre = NULL;
-        dnode->next = NULL;
+	if (dptr.head == dnode) { //dnode is first node
+		dptr.head = dptr.head->next;
+		if (dptr.head != NULL) { //dnode has next node
+			dptr.head->pre = NULL;
+			dnode->pre = NULL;
+			dnode->next = NULL;
+		}
+		else {
+			dptr.tail == NULL;
+		}
+	}
+	else if (dptr.tail == dnode) {
+		dptr.tail = dnode->pre;
+		dptr.tail->next = NULL;
 
-    }
+	}
+	else {
+		DATA *temp = dnode;
+		if (dnode->pre != NULL) {
+			dnode->pre->next = temp->next;
+		}
+		if (dnode->next != NULL) {
+			dnode->next->pre = temp->pre;
+		}
+		dnode->pre = NULL;
+		dnode->next = NULL;
 
-    free(dnode->data);
-    free(dnode);
+	}
 
-    return;
+	free(dnode->data);
+	free(dnode);
+
+	return;
 }
 
 DATA *search_data(int inode) {
 
-    if (dptr.head == NULL) {
-        return NULL;
-    }
-    DATA *curr = dptr.head;
-    while (curr != NULL && curr->d_ino < inode) {
-        if (curr->next == NULL) {
-            break;
-        } else {
-            curr = curr->next;
-        }
-    }
-    //search end
+	if (dptr.head == NULL) {
+		return NULL;
+	}
+	DATA *curr = dptr.head;
+	while (curr != NULL && curr->d_ino < inode) {
+		if (curr->next == NULL) {
+			break;
+		}
+		else {
+			curr = curr->next;
+		}
+	}
+	//search end
 
-    if (curr->d_ino == inode) { //exist
-        return curr;
-    } else {
-        return NULL; //not exist
-    }
+	if (curr->d_ino == inode) { //exist
+		return curr;
+	}
+	else {
+		return NULL; //not exist
+	}
 }
 
 DATA *make_data(int inode) {
-    DATA *n = (DATA *) calloc(1, sizeof(DATA));
-    n->d_ino = inode;
-    n->data = NULL;
-    n->pre = NULL;
-    n->next = NULL;
-    return n;
+	DATA *n = (DATA *)calloc(1, sizeof(DATA));
+	n->d_ino = inode;
+	n->data = NULL;
+	n->pre = NULL;
+	n->next = NULL;
+	return n;
 }
+
 
 
 
